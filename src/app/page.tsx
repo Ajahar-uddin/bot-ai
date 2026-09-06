e4c64b9dbaf74392454d96e3fa8902f2769992e1
Ajahar <97507958+iskajju@users.noreply.github.com>
2026-09-06 18:40:47 +0530

    Add Prisma  version 8 configuration and generated contract files, update .gitignore and package.json for new dependencies, and add Prisma Next documentation.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
-export default function Home() {
-  return <div>Hello</div>;
+import { db } from "@/prisma/db";
+
+export default async function Home() {
+  const data = await db.orm.public.User.all();
+  return <div>{JSON.stringify(data)}</div>;
 }
